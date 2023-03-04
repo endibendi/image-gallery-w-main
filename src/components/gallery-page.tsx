b26bf2404aa31dca9e0ad9d6d89d93ffc6c4d053
endibendi <endibend@gmail.com>
2023-03-04 11:52:18 +0100

    create background effect

--- src/components/gallery-page.tsx
+++ src/components/gallery-page.tsx
@@ -26,9 +26,20 @@ export const GalleryPage = () => {
   }, []);
 
   return (
-    <div tw="p-16">
-      <div tw="uppercase">XYZ Photography</div>
-      {galleryItems[0] && <Image width={250} height={250} alt="" layout="fixed" src={galleryItems[0].image} />}
+    <div tw="relative w-screen h-screen overflow-hidden">
+      <div
+        tw="z-0 absolute w-[130%] h-[130%] -translate-y-1/2 -translate-x-1/2 inset-0 bg-center bg-no-repeat blur-[100px]"
+        style={{
+          backgroundImage: `url(${galleryItems[0]?.image})`,
+          backgroundSize: "130%",
+          top: "50%",
+          left: "50%",
+        }}
+      />
+      <div tw="absolute top-0 left-0 right-0 bottom-0 p-16 z-10 w-screen h-screen">
+        <div tw="uppercase leading-[19px] text-white">XYZ Photography</div>
+        {galleryItems[0] && <Image width={250} height={250} alt="" layout="fixed" src={galleryItems[0]?.image} />}
+      </div>
     </div>
   );
 };
